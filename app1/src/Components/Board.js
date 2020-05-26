c0cf26e2ad79a97c96137983c734c7f16963bc07
syj1198 <seoyeonju1198@gmail.com>
2020-05-26 22:30:33 +0900

    5. Use npm React Component

--- app1/src/Components/Board.js
+++ app1/src/Components/Board.js
@@ -1,10 +1,32 @@
 import React, {Component} from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import "react-image-gallery/styles/css/image-gallery.css";
+import ImageGallery from 'react-image-gallery';
+ 
+const images = [
+  {
+    original: 'https://picsum.photos/id/1018/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1015/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1015/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+  },
+];
+
 
 class Board extends Component{
   render(){
     return (
       <div id='Board'>
         <h2>Board</h2>
+        <Calendar></Calendar>
+        <ImageGallery items={images} showFullscreenButton={false}></ImageGallery>
       </div>
     );
   }
